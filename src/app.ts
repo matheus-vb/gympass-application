6e651b182d40c7f27ee1697b71a757a9da9718e6
Matheus Batista <matheus.vbatista04@gmail.com>
2023-03-11 03:28:03 -0300

    fix: global error handling

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,27 @@
 import fastify from "fastify";
+import { ZodError } from "zod";
+import { env } from "./env";
 import { appRoutes } from "./http/routes";
 
 export const app = fastify();
 
 app.register(appRoutes);
+
+app.setErrorHandler((error, _, reply) => {
+    if (error instanceof ZodError) {
+        return reply.status(400).send({
+            message: "Validation failed",
+            issues: error.format(),
+        })
+    }
+
+    if (env.NODE_ENV !== "production") {
+        console.error(error);
+    } else {
+        //TODO: external log tool
+    }
+
+    return reply.status(500).send({
+        message: "Internal server error",
+    })
+})
