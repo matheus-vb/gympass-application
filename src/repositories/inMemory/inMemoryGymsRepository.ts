b5c6619474ebac86c2eb258caa803f1e7872937d
Matheus Batista <matheus.vbatista04@gmail.com>
2023-03-13 22:23:23 -0300

    feature: add gym creation to inMemory repository

--- src/repositories/inMemory/inMemoryGymsRepository.ts
+++ src/repositories/inMemory/inMemoryGymsRepository.ts
@@ -1,10 +1,8 @@
 import { Gym, Prisma } from "@prisma/client";
+import { randomUUID } from "crypto";
 import { IGymsRepository } from "../gymsRepository";
 
 export class InMemoryGymsRepository implements IGymsRepository {
-    create(data: Prisma.GymCreateInput): Promise<Gym> {
-        throw new Error("Method not implemented.");
-    }
     public gyms: Gym[] = [];
 
     async findById(id: string) {
@@ -14,6 +12,21 @@ export class InMemoryGymsRepository implements IGymsRepository {
             return null;
         }
         
+        return gym;
+    }
+
+    async create(data: Prisma.GymCreateInput): Promise<Gym> {
+        const gym: Gym = {
+            id: randomUUID(),
+            title: data.title,
+            description: data.description ?? null,
+            phone: data.phone ?? null,
+            latitude: new Prisma.Decimal(data.latitude.toString()),
+            longitude: new Prisma.Decimal(data.longitude.toString()),
+        }
+
+        this.gyms.push(gym);
+
         return gym;
     }
 }
