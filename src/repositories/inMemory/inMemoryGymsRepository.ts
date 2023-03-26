96860cbf2fc5437e7cc18d6ab165664553073294
Matheus Batista <matheus.vbatista04@gmail.com>
2023-03-26 00:30:44 -0300

    feature: add find nearby gyms to in memory repository

--- src/repositories/inMemory/inMemoryGymsRepository.ts
+++ src/repositories/inMemory/inMemoryGymsRepository.ts
@@ -1,6 +1,7 @@
+import { getDistanceBetweenCoordinates } from "@/utilities/getDistanceBetweenCoordinates";
 import { Gym, Prisma } from "@prisma/client";
 import { randomUUID } from "crypto";
-import { IGymsRepository } from "../gymsRepository";
+import { IFindManyNearbyParams, IGymsRepository } from "../gymsRepository";
 
 export class InMemoryGymsRepository implements IGymsRepository {
     public gyms: Gym[] = [];
@@ -36,5 +37,16 @@ export class InMemoryGymsRepository implements IGymsRepository {
             .slice((page - 1) * 20, page * 20)
 
         return gyms;
+    }
+
+    async findManyNearby(params: IFindManyNearbyParams) {
+        return this.gyms.filter(item => {
+            const distance = getDistanceBetweenCoordinates(
+                { latitude: params.latitude, longitude: params.longitude },
+                { latitude: item.latitude.toNumber(), longitude: item.longitude.toNumber() },
+            )
+
+            return distance < 10
+        })        
     }
 }
