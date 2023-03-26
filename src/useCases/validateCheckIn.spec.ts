9888f6c7de1b972a547dd6144edeed108d3a27b3
Matheus Batista <matheus.vbatista04@gmail.com>
2023-03-26 01:45:16 -0300

    feature: add validate check in test

---
+++ src/useCases/validateCheckIn.spec.ts
@@ -0,0 +1,39 @@
+import { InMemoryCheckInsRepository } from "@/repositories/inMemory/inMemoryCheckInsRepository";
+import { expect, describe, test, beforeEach, vi, afterEach } from "vitest";
+import { ResourceNotFoundError } from "./errors/resourceNotFoundError";
+import { ValidateCheckInUseCase } from "./validateCheckIn";
+
+let inMemoryCheckInRepository: InMemoryCheckInsRepository
+let sut: ValidateCheckInUseCase
+
+describe("Validate checkIn use case", () => {
+    beforeEach(async () => {
+        inMemoryCheckInRepository = new InMemoryCheckInsRepository();
+        sut = new ValidateCheckInUseCase(inMemoryCheckInRepository);
+
+        //vi.useFakeTimers();
+    })
+
+    afterEach(() => {
+        //vi.useRealTimers();
+    })
+
+    test("should be able to validate check-in", async () => {
+        const checkIn = await inMemoryCheckInRepository.create({
+            gymId: "Gym 01",
+            user_id: "User 01"
+        })
+        
+        await sut.execute({ checkInId: checkIn.id })
+
+        expect(checkIn.validate_at).toEqual(expect.any(Date));
+    })
+
+    test("should not be able to validate inexistent check-in", async () => {
+        await expect(() => 
+            sut.execute( {
+                checkInId: "inexistent check-in"
+            }),
+        ).rejects.toBeInstanceOf(ResourceNotFoundError)
+    })
+})
