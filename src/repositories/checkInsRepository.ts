3705c711d98b05a9ee98c81cfeba9f68135a3b37
Matheus Batista <matheus.vbatista04@gmail.com>
2023-03-26 01:21:26 -0300

    feature: add save to checkin repo interface

--- src/repositories/checkInsRepository.ts
+++ src/repositories/checkInsRepository.ts
@@ -2,6 +2,7 @@ import { Prisma, CheckIn } from "@prisma/client";
 
 export interface ICheckInsRepository {
     create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>;
+    save(checkIn: CheckIn): Promise<CheckIn>;
     findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>;
     findManyByUserId(userId: string, page: number): Promise<CheckIn[]>;
     countByUserId(userId: string): Promise<number>;
