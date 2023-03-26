150e8017bd27fffc3be6c87b7378d77a4ad80a5f
Matheus Batista <matheus.vbatista04@gmail.com>
2023-03-26 01:14:49 -0300

    feature: add find by id to checkin repo interface

--- src/repositories/checkInsRepository.ts
+++ src/repositories/checkInsRepository.ts
@@ -5,4 +5,5 @@ export interface ICheckInsRepository {
     findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>;
     findManyByUserId(userId: string, page: number): Promise<CheckIn[]>;
     countByUserId(userId: string): Promise<number>;
+    findById(id: string): Promise<CheckIn | null>;
 }
